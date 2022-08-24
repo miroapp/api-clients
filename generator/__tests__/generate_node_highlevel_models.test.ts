b9fe5818141e4327f2e657518e0b65957eccc82e
Josip Janzic <josip@miro.com>
2022-08-24 13:37:11 +0200

    Add one more test for high level model generator

--- generator/__tests__/generate_node_highlevel_models.test.ts
+++ generator/__tests__/generate_node_highlevel_models.test.ts
@@ -1,25 +1,31 @@
 import { run } from '../../generator/generate_node_highlevel_models'
+import prettier from 'prettier'
+
+function format(code: string) {
+    return prettier.format(code, {parser: 'typescript'})
+}
+
+function expectSameCode(expected: string, received: string) {
+    expect(format(expected)).toEqual(format(received))
+}
 
 describe('generate_node_highlevel_models', () => {
     it('should generate a simple model', () => {
-        expect(run({
+        expectSameCode(run({
             'Hello': {
-                id: 'Id',
-                props: ['one', 'two'],
+                id: 'id',
+                props: [],
                 methods: []
             }
-        })).toEqual(`
+        }), `
 import { MiroEndpoints } from '../api'
 import { GetParameters0, GetParameters1, GetParameters2, GetParameters3, KeepBase, toString } from "./helpers";
-
-
-
 
 export class Hello extends Object {
     /** @hidden */
     _api: MiroEndpoints
     /** @hidden */
-    _headParams: [string, string]
+    _headParams: []
 
     constructor(api: MiroEndpoints, headParams: Hello['_headParams'], rest: object) {
         super()
@@ -27,9 +33,69 @@ export class Hello extends Object {
         this._headParams = headParams
         Object.assign(this, rest)
     }
+}
+`)
+    })
 
+    it('should correctly generate a model with a getItem method', () => {
+        expectSameCode(run({
+            'Hello': {
+                id: 'id',
+                props: [],
+                methods: [
+                    {method: 'getSomeItem', alias: 'getItem', returns: 'Item', topLevelCall: false}
+                ]
+            },
+            'Item': {
+                id: 'itemId',
+                props: [],
+                methods: []
+            }
+        }), `
 
+import { MiroEndpoints } from '../api'
+import { GetParameters0, GetParameters1, GetParameters2, GetParameters3, KeepBase, toString } from "./helpers";
 
+export class Hello extends Object {
+  /** @hidden */
+  _api: MiroEndpoints
+  /** @hidden */
+  _headParams: []
+
+  constructor(api: MiroEndpoints, headParams: Hello['_headParams'], rest: object) {
+    super()
+    this._api = api
+    this._headParams = headParams
+    Object.assign(this, rest)
+  }
+
+  /** {@inheritDoc api!MiroEndpoints.getSomeItem} */
+  async getItem(
+    ...rest: GetParameters0<MiroEndpoints["getSomeItem"]>
+  ): Promise<Item> {
+    const result = (await this._api.getSomeItem(...this._headParams, ...rest))
+      .body;
+
+    return new Item(this._api, [], result);
+  }
+}
+
+export class Item extends Object {
+  /** @hidden */
+  _api: MiroEndpoints;
+  /** @hidden */
+  _headParams: [];
+
+  constructor(
+    api: MiroEndpoints,
+    headParams: Item["_headParams"],
+    rest: object
+  ) {
+    super();
+    this._api = api;
+    this._headParams = headParams;
+    Object.assign(this, rest);
+  }
 }
 `)
     })
