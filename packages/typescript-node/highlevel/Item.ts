3664d2f19045194d36dc7005b679afb49e303d2a
Josip Janzic <josip@miro.com>
2022-08-31 10:22:15 +0200

    Implement connectTo method on the Item

--- packages/typescript-node/highlevel/Item.ts
+++ packages/typescript-node/highlevel/Item.ts
@@ -1,8 +1,18 @@
 import {GenericItem} from '../model/genericItem'
 import {MiroApi} from '../api'
+import {Connector} from '.'
 
 export abstract class Item extends GenericItem {
   abstract _api: MiroApi
+  abstract boardId: string | undefined
 
-  // custom methods go here
+  async connectTo(endItemId: string | number): Promise<Connector> {
+    const connector = (
+      await this._api.createConnector(this.boardId?.toString() || '', {
+        startItem: {id: this.id?.toString() || ''},
+        endItem: {id: endItemId.toString()},
+      })
+    ).body
+    return new Connector(this._api, this.boardId, connector.id, connector)
+  }
 }
