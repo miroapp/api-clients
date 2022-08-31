65c7706d4c042e210fc6666d04f51300004da9f1
Josip Janzic <josip@miro.com>
2022-08-31 10:22:15 +0200

    Implement connectTo method on the Item

---
+++ packages/typescript-node/highlevel/__tests__/Item.test.ts
@@ -0,0 +1,24 @@
+import {MiroApi} from '../../api'
+import {Connector, Item} from '../index'
+
+describe('Item test', () => {
+  describe('connectTo', () => {
+    it('calls the api with expected parameters', async () => {
+      const api = new MiroApi('token')
+      api.createConnector = jest.fn(async () => ({body: {}, response: {} as any}))
+
+      const startItemId = 123
+      const endItemId = 456
+
+      const item = new Item(api, 'boardId', startItemId, {})
+      const connector = await item.connectTo(endItemId)
+
+      expect(api.createConnector).toBeCalledWith('boardId', {
+        startItem: {id: startItemId.toString()},
+        endItem: {id: endItemId.toString()},
+      })
+
+      expect(connector).toBeInstanceOf(Connector)
+    })
+  })
+})
