6f79673ce0d9424dca5215d230b8452520258aea
Mettin Parzinski <mettin@miro.com>
2022-09-01 14:41:07 +0200

    Added getItems method

--- packages/typescript-node/highlevel/FrameItem.ts
+++ packages/typescript-node/highlevel/FrameItem.ts
@@ -1,8 +1,27 @@
 import {FrameItem as BaseFrameItem} from '../model/frameItem'
 import {MiroApi} from '../api'
+import {Item} from './index'
+
+export type WidgetTypes = "app_card" | "card" | "document" | "embed" | "frame" | "image" | "shape" | "sticky_note" | "text"
+type Items = {
+  limit?: string
+  type?: WidgetTypes
+  cursor?: string
+}
 
 export abstract class FrameItem extends BaseFrameItem {
   abstract _api: MiroApi
+  abstract id: number
+  abstract boardId: string
 
-  // custom methods go here
+  async getItems(params?: Items) {
+    const {body} = await this._api.getItemsWithinFrame(this.boardId, String(this.id), params)
+
+    if (!body.data) return body
+
+    return {
+      ...body,
+      data: body.data.map(item => new Item(this._api, this.boardId, item.id, item))
+    }
+  }
 }
