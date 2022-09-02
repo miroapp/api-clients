46e79362353f34cdb1a98ea6393e4508b32a0b2c
Mettin Parzinski <mettin@miro.com>
2022-09-02 11:52:13 +0200

    Explain what this script does a bit better since I was unable to read it myself

--- test/generator_test.sh
+++ test/generator_test.sh
@@ -3,9 +3,13 @@ set -euo pipefail
 
 yarn generate
 
+# Check if there are uncommitted changes
+# git status -s = list changes, 1 changed file per line
+# git status -s | wc -l = Count number of lines, and thus changed files
+# git status -s | wc -l | ts -d ' ' = remove spaces
 if [[ $(git status -s | wc -l | tr -d ' ') != 0 ]]; then
     echo "FAILED: "
-    echo "  There are some differences between the generated code and commited code, see below: "
+    echo "  There are some differences between the generated code and committed code, see below: "
     echo
     git status -s
     exit 1
