b8f0ed51a0ea367760ed227e25cecc8aa1357af2
Mettin Parzinski <mettin@miro.com>
2022-09-12 13:46:52 +0200

    Setup of test app

---
+++ apps/client-test-app/index.js
@@ -0,0 +1,51 @@
+import express from 'express'
+import {Miro} from '@mirohq/miro-node'
+
+const app = express()
+
+const miro = new Miro({
+  clientId: '',
+  clientSecret: '',
+  redirectUrl: 'https://plenty-bears-buy-208-127-180-6.loca.lt/auth/miro/callback',
+})
+const USER_ID = 123
+
+app.get('/login', async (req, res) => {
+  if (await miro.isAuthorized(USER_ID)) {
+    await miro.handleAuthorizationCodeRequest(USER_ID, req)
+    res.redirect('/')
+    return
+  }
+  res.redirect(miro.getAuthUrl())
+})
+
+app.get('/auth/miro/callback/', async (req, res) => {
+  try {
+    await miro.handleAuthorizationCodeRequest(USER_ID, req)
+  } catch (e) {
+    console.log('handleAuthorizationCodeRequest failed:', e)
+  }
+
+  res.redirect('/')
+})
+
+app.get('/', async (req, res) => {
+  const b = await miro.isAuthorized(USER_ID)
+  if (!b) {
+    res.redirect('/login')
+    return
+  }
+  const api = miro.as(USER_ID)
+  const boards = await api.getBoards()
+
+  res.send(
+    'Your boards:' +
+      boards
+        .map((board) => {
+          return `<br><a href="${board.viewLink}">${board.name}</a>`
+        })
+        .join(''),
+  )
+})
+
+app.listen(3000, () => console.log(`Listening on localhost, port 3000. http://localhost:3000`))
