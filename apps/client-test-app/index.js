2a346cd444ff912fe9c910314025cd2cfd509c32
Mettin Parzinski <mettin@miro.com>
2022-09-16 14:42:35 +0200

    Made the test app a more real app, where you can navigate through boards and inspect its contents. removed hardcoded IDs

--- apps/client-test-app/index.js
+++ apps/client-test-app/index.js
@@ -1,22 +1,26 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import {Miro} from '@mirohq/miro-node'
 
 const app = express()
 
-const miro = new Miro({
-  clientId: '3458764532875095934',
-  clientSecret: 'SgoAjkPYfF9NaHcVRevnY6GYwzfjtgtU',
-  redirectUrl: 'https://plenty-bears-buy-208-127-180-6.loca.lt/auth/miro/callback',
-})
-const USER_ID = 3458764514451650476
-const TEST_BOARD_ID = 'uXjVPZcwwIY='
-const TEST_ITEM_ID = '3458764533355792545'
-const TEST_FRAME_ID = '3458764533388333263'
-const TEST_TEAM_ID = '3458764515148204448'
+const options = {
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUrl: `https://${process.env.TUNNEL}.loca.lt/auth/miro/callback`,
+}
+const miro = new Miro(options)
+
+const USER_ID = 'WE_DONT_NEED_A_REAL_ID'
 
 app.get('/login', async (req, res) => {
   if (await miro.isAuthorized(USER_ID)) {
-    await miro.handleAuthorizationCodeRequest(USER_ID, req)
+    try {
+      await miro.handleAuthorizationCodeRequest(USER_ID, req)
+    } catch (e) {
+      console.error('Error with handleAuthorizationCodeRequest', e)
+    }
     res.redirect('/')
     return
   }
@@ -37,106 +41,86 @@ app.get('/', async (req, res) => {
     res.redirect('/login')
     return
   }
-
   const api = miro.as(USER_ID)
 
-  let body = ''
-
-  body += await testHighLevelApi(api.getAllBoards())
-  body += await testHighLevelBoard(api)
-  body += await testHighLevelFrameItem(api)
-  await testHighLevelItem(api)
-  body += await testHighLevelTeam(api)
+  let body = '<h1>All boards you have access to</h1><ul>'
+  for await (const board of api.getAllBoards()) {
+    body += `<li><a href="boards/${board.id}">${board.name} (${board.id})</a></li>`
+  }
+  body += '</ul>'
 
   res.send(body)
 })
 
+app.get('/boards/:boardId/item/:itemId', async (req, res) => {
+  if (!(await miro.isAuthorized(USER_ID))) {
+    res.redirect('/login')
+    return
+  }
+
+  const boardId = req.params.boardId
+  const itemId = req.params.itemId
+  const api = miro.as(USER_ID)
+  const board = await api.getBoard(boardId)
+  const item = await board.getItem(itemId)
+
+  let body = `
+    <h1><a href="/boards/${boardId}" title="Back to board">${board.name}</a> / ${item.id}</h1>
+    <dl>
+        <dt>id:</dt>
+        <dd>${item.id}</dd>
+        <dt>type:</dt>
+        <dd>${item.type}</dd>
+        <dt>id:</dt>
+        <dd>${item.id}</dd>
+        <dt>Board id:</dt>
+        <dd>${item.boardId}</dd>
+        <dt>data.content:</dt>
+        <dd>${item.data?.content}</dd>
+        <dt>geometry:</dt>
+        <dd><pre>${JSON.stringify(item.geometry, null, 2)}</pre></dd>
+        <dt>modifiedAt:</dt>
+        <dd>${item.modifiedAt}</dd>
+        <dt>modifiedBy:</dt>
+        <dd><pre>${JSON.stringify(item.modifiedBy, null, 2)}</pre></dd>
+        <dt>position:</dt>
+        <dd><pre>${JSON.stringify(item.position, null, 2)}</pre></dd>
+    </dl>
+    `
+
+  res.send(body)
+})
+
+app.get('/boards/:boardId', async (req, res) => {
+  if (!(await miro.isAuthorized(USER_ID))) {
+    res.redirect('/login')
+    return
+  }
+
+  const boardId = req.params.boardId
+  const api = miro.as(USER_ID)
+  const board = await api.getBoard(boardId)
+  const boardItemsGenerator = await board.getAllItems()
+  const boardItems = []
+
+  for await (const item of boardItemsGenerator) {
+    boardItems.push(item)
+  }
+  console.log(boardItems)
+
+  let body = `
+    <h1>${board.name} / ${boardId}</h1>
+    <a href="/">Back to all boards</a>
+    <ul>
+        ${boardItems
+          .map(
+            (item) =>
+              `<li>ID: <em><a href="/boards/${boardId}/item/${item.id}">${item.id}</a></em>  (${item.type})</li>`,
+          )
+          .join('')}
+    </ul>
+  `
+
+  res.send(body)
+})
 app.listen(3000, () => console.log(`Listening on localhost, port 3000. http://localhost:3000`))
-
-const testHighLevelApi = async (allBoards) => {
-  // Test if api.getAllBoards() is working
-  let response = '<h1>All boards: (fetched by <code>api.getAllBoards()</code>)</h1><ul>'
-  for await (const board of allBoards) {
-    response += `<li><a href="${board.viewLink}">${board.name} (${board.id})</a></li>`
-  }
-  response += '</ul>'
-
-  return response
-}
-const testHighLevelBoard = async (api) => {
-  // Test if board.getAllItems() is working
-  let response = `<h1>Board items</h1>`
-  for await (const board of api.getAllBoards()) {
-    if (board.id !== TEST_BOARD_ID) continue
-    response += `<h2>${board.name} (${board.id})</h2><ul>`
-    for await (const item of board.getAllItems()) {
-      response += `<li>id: <em>${item.id}</em> / type: <em>${item.type}</em></li>`
-    }
-    response += '</ul>'
-  }
-
-  // Test if board.getItem() is working
-  response += `<h1>board.getItem(${TEST_ITEM_ID})</h1>`
-  for await (const board of api.getAllBoards()) {
-    if (board.id !== TEST_BOARD_ID) continue
-    const item = await board.getItem(TEST_ITEM_ID)
-    response += `<ul>
-        <li>ID: <em>${item.id}</em></li>
-        <li>Type: <em>${item.type}</em></li>
-        <li>Text: <em>${item.data.content}</em></li>
-        <li>Geometry: <em>${JSON.stringify(item.geometry, null, 2)}</em></li>
-        <li>Modified at: <em>${new Date(item.modifiedAt)}</em></li>
-    </ul>`
-  }
-
-  return response
-}
-function getAllFuncs(toCheck) {
-  const props = []
-  let obj = toCheck
-  do {
-    props.push(...Object.getOwnPropertyNames(obj))
-  } while ((obj = Object.getPrototypeOf(obj)))
-
-  return props.sort().filter((e, i, arr) => {
-    if (e !== arr[i + 1] && typeof toCheck[e] == 'function') return true
-  })
-}
-
-const testHighLevelFrameItem = async (api) => {
-  let response = ''
-
-  for await (const board of api.getAllBoards()) {
-    if (board.id !== TEST_BOARD_ID) continue
-    /*
-     * TODO:
-     *  We dont have a highlevel API yet that returns a frameItem class so we cant use FrameItem yet
-     *  */
-    const frameItem = await board.getFrameItem(TEST_FRAME_ID)
-  }
-
-  return response
-}
-const testHighLevelItem = async (api) => {
-  for await (const board of api.getAllBoards()) {
-    if (board.id !== TEST_BOARD_ID) continue
-
-    let firstItemId
-    for await (const item of board.getAllItems()) {
-      if (!firstItemId) {
-        firstItemId = item.id
-        continue
-      }
-      await item.connectTo(firstItemId)
-      break
-    }
-  }
-  return ''
-}
-const testHighLevelTeam = async (api) => {
-  /* TODO: api.getTeams() isnt working yet */
-  // for (const team of api.getTeams()) {
-  // console.log(team)
-  // }
-  return ''
-}
