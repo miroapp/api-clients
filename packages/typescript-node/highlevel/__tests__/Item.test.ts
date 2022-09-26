911d1f22c94934d503faaa15655385480da354ac
Mettin Parzinski <mettin@miro.com>
2022-09-26 13:43:23 +0200

    Added tests for different inputs in connectTo

--- packages/typescript-node/highlevel/__tests__/Item.test.ts
+++ packages/typescript-node/highlevel/__tests__/Item.test.ts
@@ -4,20 +4,79 @@ import {jest} from '@jest/globals'
 
 describe('Item test', () => {
   describe('connectTo', () => {
-    it('calls the api with expected parameters', async () => {
+    const now = new Date().toUTCString()
+    it.each([
+      [
+        'plain number input for endpoint',
+        [456],
+        {
+          startItem: {id: '123'},
+          endItem: {id: '456'},
+        },
+      ],
+      [
+        'object input for endPoint',
+        [{id: '456', snapTo: 'top'}],
+        {
+          startItem: {id: '123'},
+          endItem: {id: '456', snapTo: 'top'},
+        },
+      ],
+      [
+        'object with modifications for endpoint',
+        [{id: '456', position: {x: '10%', y: '87%'}}],
+        {
+          startItem: {id: '123'},
+          endItem: {id: '456', position: {x: '10%', y: '87%'}},
+        },
+      ],
+      [
+        'snapTo modifier for endpoint',
+        [456, {snapTo: 'top'}],
+        {
+          startItem: {id: '123', snapTo: 'top'},
+          endItem: {id: '456'},
+        },
+      ],
+      [
+        'extra options for line',
+        [
+          456,
+          {},
+          {
+            style: {
+              color: '#9510ac',
+            },
+            shape: 'elbowed',
+            captions: [{content: now}, {content: 'whoa'}],
+          },
+        ],
+        {
+          endItem: {id: '456'},
+          startItem: {
+            id: '123',
+          },
+          captions: [{content: now}, {content: 'whoa'}],
+          shape: 'elbowed',
+          style: {
+            color: '#9510ac',
+          },
+        },
+      ],
+    ])('calls the api with expected parameters (%s)', async (_, input: any[], expectedArguments) => {
       const api = new MiroApi('token')
-      api.createConnector = jest.fn(async () => ({body: {}, response: {} as any}))
+      api.createConnector = jest.fn(async () => ({
+        body: {id: '789'},
+        response: {} as any,
+      }))
 
       const startItemId = 123
-      const endItemId = 456
 
       const item = new Item(api, 'boardId', startItemId, {})
-      const connector = await item.connectTo(endItemId)
+      // @ts-ignore
+      const connector = await item.connectTo(...input)
 
-      expect(api.createConnector).toBeCalledWith('boardId', {
-        startItem: {id: startItemId.toString()},
-        endItem: {id: endItemId.toString()},
-      })
+      expect(api.createConnector).toBeCalledWith('boardId', expectedArguments)
 
       expect(connector).toBeInstanceOf(Connector)
     })
