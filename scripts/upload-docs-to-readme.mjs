7dd215a195060bd4f19f3683c3bae2be4062bd30
Josip Janzic <josip@miro.com>
2022-09-28 22:38:04 +0200

    DX-1235 Added script to upload the docs to ReadMe

---
+++ scripts/upload-docs-to-readme.mjs
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import fetch from 'node-fetch'
+import fs from 'fs'
+
+const key = process.env.README_API_KEY
+if (!key) throw new Error('Missing environment variable: README_API_KEY')
+
+// Readme version of the site to publish to
+const readmeVersion = '2-nodejs-test'
+
+// ID of the Miro REST API Clients category for the guides
+const categoryId = '6334152e20e40f07e4f23e6b'
+
+async function updateDoc(slug, title, category, file) {
+  const res = await fetch(`https://dash.readme.com/api/v1/docs/${slug}`, {
+    method: 'PUT',
+    headers: {
+      'x-readme-version': readmeVersion,
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${Buffer.from(key, 'utf-8').toString('base64')}`
+    },
+    body: JSON.stringify({ title, category, body: fs.readFileSync(file, 'utf-8') })
+  })
+  console.log('Uploaded', (await res.json()).title)
+}
+
+await updateDoc(
+  'node-api-client',
+  'Node.js Api Client',
+  categoryId,
+  './packages/typescript-node/README.md'
+)
