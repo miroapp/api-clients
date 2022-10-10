01007353396e175c5280b5b7f85bc77ea363e0cc
Josip Janzic <josip@miro.com>
2022-10-10 11:03:50 +0200

    DX-1239 Replace includedoc with inline docs
    This fixes the issue with documentation for method parameters, where the
    documentation would be showing wrong parameter descriptions.
    
    Model generator now uses typescript parser to copy the code from the
    lowlevel methods into the highlevel methods, and generate human readable
    parameter names.

--- packages/typescript-node/highlevel/helpers.ts
+++ packages/typescript-node/highlevel/helpers.ts
@@ -12,40 +12,6 @@ export function hasMoreData(response: {offset?: number; data?: Array<any>; total
   return !!total && !!size && responseOffset + size < total
 }
 
-/** @hidden */
-export type GetParameters0<Method extends (p1: any, ...rest: any[]) => any> = Method extends (
-  ...rest: infer Rest
-) => any
-  ? Rest
-  : never
-
-/** @hidden */
-export type GetParameters1<Method extends (p1: any, ...rest: any[]) => any> = Method extends (
-  p1: any,
-  ...rest: infer Rest
-) => any
-  ? Rest
-  : never
-
-/** @hidden */
-export type GetParameters2<Method extends (p1: any, p2: any, ...rest: any[]) => any> = Method extends (
-  p1: any,
-  p2: any,
-  ...rest: infer Rest
-) => any
-  ? Rest
-  : never
-
-/** @hidden */
-export type GetParameters3<Method extends (p1: any, p2: any, p3: any, ...rest: any[]) => any> = Method extends (
-  p1: any,
-  p2: any,
-  p3: any,
-  ...rest: infer Rest
-) => any
-  ? Rest
-  : never
-
 type DeepPartial<T> = T extends object
   ? {
       [P in keyof T]?: T[P] extends string ? string : DeepPartial<T[P]>
