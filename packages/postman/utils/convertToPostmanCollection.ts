58fedf611366753ae2571a1353522d864356de05
Raktima <raktima@miro.com>
2023-12-18 17:29:01 +0100

    added commented code to enable selection of optional params in postman

--- packages/postman/utils/convertToPostmanCollection.ts
+++ packages/postman/utils/convertToPostmanCollection.ts
@@ -1,10 +1,10 @@
 import Converter from 'openapi-to-postmanv2'
 import fs from 'fs'
-import {missingPostmanVariables, auth, event} from './sample.constants'
+import { missingPostmanVariables, auth, event } from './sample.constants'
 
-export default ({type = 'json', oas}) =>
+export default ({ type = 'json', oas }) =>
   Converter.convert(
-    {type, data: oas},
+    { type, data: oas },
     {
       requestParametersResolution: 'Example',
       folderStrategy: 'Tags',
@@ -22,9 +22,35 @@ export default ({type = 'json', oas}) =>
           ...auth,
           ...event,
         }
+
+        // const modifiedPostmanJson = unselectOptionalQueryParams(postmanCollectionJson.item);
+
         // TODO: send postman collection to api
-        fs.writeFileSync('generated/postman-collection.json', JSON.stringify(postmanCollectionJson))
+        fs.writeFileSync('generated/postman-collection.json', JSON.stringify(postmanCollectionJson, null, 2))
         console.log('Postman collection generated at : generated/postman-collection.json ')
       }
     },
   )
+
+// NOTE: hack to disable (unselect) all optional query params in postman collection 
+// so that requests with optional params work directly in postman
+// openapi-to-postmanv2 doesn't seem to provide an ootb way to do this
+// const unselectOptionalQueryParams = (items) => {
+//   for (const item of items) {
+//     if (Array.isArray(item)) {
+//       item.forEach((o) => {
+//         if (o.item) unselectOptionalQueryParams(o.item);
+
+//         if (o.url && o.url.query) {
+//           const allQueries = o.url.query.map((q) => !q.description.toLowerCase().startsWith("(Required)".toLowerCase()) ? { ...q, disabled: true } : q)
+//           o.url.query = allQueries;
+//         }
+
+// TODO: consider "parameters" also for unselcting optional query params
+//       })
+//     }
+//   }
+
+
+//   return items;
+// }
