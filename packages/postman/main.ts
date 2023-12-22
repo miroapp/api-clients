b065b32962f5e4337e279f8384ecf1ce733838de
raktima <140501295+imxena@users.noreply.github.com>
2023-12-22 14:08:11 +0100

    Update packages/postman/main.ts
    Co-authored-by: Mettin Parzinski <mettin@live.nl>

--- packages/postman/main.ts
+++ packages/postman/main.ts
@@ -5,15 +5,13 @@ import {OpenAPIV3_1} from 'openapi-types'
 import {writeModifiedOASFile} from './utils/writeModifiedOASFile'
 import {convertOASToPostmanCollection} from './utils/convertOASToPostmanCollection'
 
-function traverseOASAndComponents(oas: any) {
-  traverseOAS(oas)
-  traverseComponents(oas)
-}
-
 try {
   const oasFile: string = fs.readFileSync('./../generator/spec.json', 'utf-8')
   const oas: OpenAPIV3_1.Document = JSON.parse(oasFile)
-  traverseOASAndComponents(oas)
+  
+  traverseOAS(oas)
+  traverseComponents(oas)
+  
   writeModifiedOASFile(oas, 'generated/spec-modified.json')
   convertOASToPostmanCollection(oas)
 } catch (error) {
