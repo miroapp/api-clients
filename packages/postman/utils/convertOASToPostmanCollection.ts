2b957b60b144372b688dda02e42881630a7e1644
Raktima <raktima@miro.com>
2023-12-27 10:11:31 +0100

    changed type

--- packages/postman/utils/convertOASToPostmanCollection.ts
+++ packages/postman/utils/convertOASToPostmanCollection.ts
@@ -1,6 +1,7 @@
 import convertToPostmanCollection from './convertToPostmanCollection'
+import {OpenAPIV3_1} from './../types/types'
 
-export function convertOASToPostmanCollection(oas: any) {
+export function convertOASToPostmanCollection(oas: OpenAPIV3_1.Document) {
   const modifiedOasFile = JSON.stringify(oas, null, 2)
   convertToPostmanCollection({oas: modifiedOasFile})
 }
