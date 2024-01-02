3f8c1b0e3c4490afd84a406dae1056a7c28b7760
raktima <140501295+imxena@users.noreply.github.com>
2024-01-02 11:27:14 +0100

    Update packages/postman/utils/string-utils.ts
    Co-authored-by: Kirill Sudarushkin <kirill.su@miro.com>

--- packages/postman/utils/string-utils.ts
+++ packages/postman/utils/string-utils.ts
@@ -2,7 +2,7 @@ export function camelToSnake(str: string) {
   return str.replace(/[A-Z]/g, (match) => `_${match.toLowerCase()}`)
 }
 
-export const snakeToTitleCase = (str) => {
+export function snakeToTitleCase(str: string) {
   const words = str.split('_')
   const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
   return capitalizedWords.join(' ')
