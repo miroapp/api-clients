109dc90a5944e18acdf4d2aef530213740204d00
Josip Janzic <josip@miro.com>
2024-04-09 12:59:02 +0200

    DX-2848 Fix build of docs (#358)

--- packages/miro-api-python/scripts/build-docs.sh
+++ packages/miro-api-python/scripts/build-docs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 #
 poetry run pdoc miro_api \
-    -d markdown -t ./docs-template -o docs-out \
     '!miro_api.rest' \
     '!miro_api.api_response' \
     '!miro_api.client' \
@@ -10,4 +9,5 @@ poetry run pdoc miro_api \
     '!miro_api.test_miro_api_wrapper' \
     '!miro_api.api_client' \
     '!miro_api.miro_api_wrapper' \
-    '!miro_api.exceptions'
+    '!miro_api.exceptions' \
+    -d markdown -t ./docs-template -o docs-out 
